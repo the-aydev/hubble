c6d76f5020375f310794b30718ec705473ee1efa
Ayomide <ayomidedans@gmail.com>
2023-04-02 23:51:22 +0100

    hubble design

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { ThemeProvider } from "styled-components";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Card from "./components/Card";
 import { Container } from "./components/styles/Container.styled";
 import GlobalStyles from "./components/styles/Global";
+import content from "./content";
 
 const theme = {
   colors: {
     header: '#ebfbff',
     body: '#fff',
     footer: '#003333'
-  }
+  },
+  mobile: '768px',
 };
 
 function App () {
@@ -18,8 +22,11 @@ function App () {
         <GlobalStyles />
         <Header />
         <Container>
-          <h1>Hello Huddle</h1>
+          {content.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
         </Container>
+        <Footer />
       </>
     </ThemeProvider>
   );
--- src/components/styles/Card.styled.js
+++ src/components/styles/Card.styled.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const StyledCard = styled.div`
   display: flex;
@@ -21,4 +21,4 @@ export const StyledCard = styled.div`
   @media (max-width: ${({ theme }) => theme.mobile}) {
     flex-direction: column;
   }
-`
+`;
